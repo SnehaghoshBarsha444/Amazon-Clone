913622b7a7d67913586f628dc90bb6b600ff04e5
Sneha Ghosh (Technical_Isopod_075) <153223798+SnehaghoshBarsha444@users.noreply.github.com>
2024-12-26 17:38:11 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,23 +39,23 @@ export default function Home({ productData }: Props) {
           <div className="left-btn"><i className="fa-solid fa-chevron-left"></i></div>
           <div className="right-btn"><i className="fa-solid fa-chevron-right"></i></div>
           <ul className="slide-carousel">
-            <li><img src="/amazon assets/bed1.png" className="slide-img" alt="bed1" /></li>
-            <li><img src="/amazon assets/bed2.png" className="slide-img" alt="bed2" /></li>
-            <li><img src="/amazon assets/bed3.png" className="slide-img" alt="bed3" /></li>
-            <li><img src="/amazon assets/bed4.png" className="slide-img" alt="bed4" /></li>
-            <li><img src="/amazon assets/bed5.png" className="slide-img" alt="bed5" /></li>
-            <li><img src="/amazon assets/bed6.png" className="slide-img" alt="bed6" /></li>
-            <li><img src="/amazon assets/bed7.png" className="slide-img" alt="bed7" /></li>
-            <li><img src="/amazon assets/bed8.png" className="slide-img" alt="bed8" /></li>
-            <li><img src="/amazon assets/bed9.png" className="slide-img" alt="bed9" /></li>
-            <li><img src="/amazon assets/bed10.png" className="slide-img" alt="bed10" /></li>
-            <li><img src="/amazon assets/bed11.png" className="slide-img" alt="bed11" /></li>
-            <li><img src="/amazon assets/bed12.png" className="slide-img" alt="bed12" /></li>
-            <li><img src="/amazon assets/bed13.png" className="slide-img" alt="bed13" /></li>
-            <li><img src="/amazon assets/bed14.png" className="slide-img" alt="bed14" /></li>
-            <li><img src="/amazon assets/bed16.png" className="slide-img" alt="bed16" /></li>
-            <li><img src="/amazon assets/bed17.png" className="slide-img" alt="bed17" /></li>
-            <li><img src="/amazon assets/bed18.png" className="slide-img" alt="bed18" /></li>
+            <li><img src="amazon assets/bed1.png" className="slide-img" alt="bed1" /></li>
+            <li><img src="amazon assets/bed2.png" className="slide-img" alt="bed2" /></li>
+            <li><img src="amazon assets/bed3.png" className="slide-img" alt="bed3" /></li>
+            <li><img src="amazon assets/bed4.png" className="slide-img" alt="bed4" /></li>
+            <li><img src="amazon assets/bed5.png" className="slide-img" alt="bed5" /></li>
+            <li><img src="amazon assets/bed6.png" className="slide-img" alt="bed6" /></li>
+            <li><img src="amazon assets/bed7.png" className="slide-img" alt="bed7" /></li>
+            <li><img src="amazon assets/bed8.png" className="slide-img" alt="bed8" /></li>
+            <li><img src="amazon assets/bed9.png" className="slide-img" alt="bed9" /></li>
+            <li><img src="amazon assets/bed10.png" className="slide-img" alt="bed10" /></li>
+            <li><img src="amazon assets/bed11.png" className="slide-img" alt="bed11" /></li>
+            <li><img src="amazon assets/bed12.png" className="slide-img" alt="bed12" /></li>
+            <li><img src="amazon assets/bed13.png" className="slide-img" alt="bed13" /></li>
+            <li><img src="amazon assets/bed14.png" className="slide-img" alt="bed14" /></li>
+            <li><img src="amazon assets/bed16.png" className="slide-img" alt="bed16" /></li>
+            <li><img src="amazon assets/bed17.png" className="slide-img" alt="bed17" /></li>
+            <li><img src="amazon assets/bed18.png" className="slide-img" alt="bed18" /></li>
           </ul>
         </div>
         {/* Other Components */}
